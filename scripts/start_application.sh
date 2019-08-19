7baced5065e08884ac44fb517fa8675b8e1a4964
Abdel <abdelrahman.elbarbary@gmail.com>
2019-08-19 11:30:42 -0700

    add nginx config

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,5 +1,5 @@
 # sudo python3 /home/ubuntu/www/tamkeen/app/manage.py runserver 0.0.0.0:80 > /dev/null 2>&1 &
 sudo systemctl start nginx
 
-sudo kill -9 $(sudo lsof -t -i:8003)
-sudo uwsgi --chdir /home/ubuntu/www/nozolan.com/app --socket :8003 --module tamkeen.wsgi > /dev/null 2>&1 &
+sudo kill -9 $(sudo lsof -t -i:8004)
+sudo uwsgi --chdir /home/ubuntu/www/nozolan/app --socket :8004 --module nozolan.wsgi > /dev/null 2>&1 &
